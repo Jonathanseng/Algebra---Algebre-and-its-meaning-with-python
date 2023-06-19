8cad2a6813e230f3092cafb045e1aa0c63cc9013
Seng Keat <corodostudio@gmail.com>
2023-06-19 11:38:12 +0700

    Update 11.2 Summation.py

--- 11. Summation/11.2 Summation.py
+++ 11. Summation/11.2 Summation.py
@@ -25,3 +25,15 @@ if __name__ == "__main__":
 #The main function of the code then assigns the values 1 and 10 to the starting and ending values of the sum, respectively. It then calls the summation() function to calculate the sum of the numbers from 1 to 10. The result of the calculation is then printed to the console.
 
 #To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as summation.py, you can run it by typing the following command into the command line:
+
+#This will print the value of the sum to the console.
+
+#Here is an explanation of the code:
+
+#The def keyword is used to define a function.
+#The summation() function takes two arguments: start and end.
+#The range() function is used to iterate over a sequence of numbers.
+#The for loop is used to iterate over the sequence of numbers.
+#The sum += i statement adds the value of i to the sum variable.
+#The return sum statement returns the value of the sum variable.
+#The if __name__ == "__main__": statement is used to execute the code in the main function when the file is run as a script.
