f28786742e0015eb92729e6ae6a5caca0ba328ec
Seng Keat <corodostudio@gmail.com>
2023-06-19 09:56:17 +0700

    Create 6.2 FunctionOfX.py

---
+++ 6. Function Of X/6.2 FunctionOfX.py
@@ -0,0 +1,17 @@
+def f(x):
+  """Returns the square of x."""
+  return x * x
+
+print(f(3))
+
+# This code defines a function named f that takes an input value, x, and returns the square of x. The function is defined using function notation, and the output value is calculated using the * operator. The code then prints the output value of the function for the input value 3.
+
+#To run this code, you can save it as a file named function_of_x.py and then run it from the command line using the following command:
+#python function_of_x.py
+# This will print the output value of the function, which is 9.
+
+#Here is an explanation of the code:
+
+#The first line defines the function f. The function takes an input value, x, and returns the square of x.
+#The second line defines the output value of the function. The output value is calculated using the * operator.
+#The third line prints the output value of the function for the input value 3.
