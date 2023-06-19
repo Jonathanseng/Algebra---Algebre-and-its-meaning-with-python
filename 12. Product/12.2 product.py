f3681b3b580a99655c8a44858756a8f5e16bc95e
Seng Keat <corodostudio@gmail.com>
2023-06-19 13:56:12 +0700

    Create 12.2 product.py

---
+++ 12. Product/12.2 product.py
@@ -0,0 +1,28 @@
+def product(x, y):
+  """
+  Calculates the product of two numbers.
+
+  Args:
+    x: The first number.
+    y: The second number.
+
+  Returns:
+    The product of x and y.
+  """
+
+  return x * y
+
+
+if __name__ == "__main__":
+  print(product(2, 5))
+  print(product(3, -4))
+  print(product(2, 3.5))
+
+  #This code first defines a function called product() that takes two numbers as input and returns the product of those numbers. The function then uses the return statement to return the product of the two numbers.
+
+#The if __name__ == "__main__": block at the end of the code is used to execute the code if it is being run as a script. If the code is being imported into another program, this block will not be executed.
+
+#The code then prints the product of 2 and 5, the product of 3 and -4, and the product of 2 and 3.5.
+
+#To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as product.py, you can run it by typing the following command into the command line:
+
