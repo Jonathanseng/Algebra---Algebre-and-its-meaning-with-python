57612847dc754852dde71f466a0df42c3339d846
Seng Keat <corodostudio@gmail.com>
2023-06-19 13:58:31 +0700

    Create 13.2 Infinity.py

---
+++ 13. Infinity/13.2 Infinity.py
@@ -0,0 +1,23 @@
+def infinity():
+  """
+  Prints the positive and negative infinity in Python.
+
+  Returns:
+    None.
+  """
+
+  print("Positive infinity:", float("inf"))
+  print("Negative infinity:", float("-inf"))
+
+
+if __name__ == "__main__":
+  infinity()
+
+  # This code first defines a function called infinity() that prints the positive and negative infinity in Python. The function then uses the float() function to convert the strings "inf" and "-inf" to floating-point numbers. These floating-point numbers represent positive and negative infinity, respectively.
+
+#The if __name__ == "__main__": block at the end of the code is used to execute the code if it is being run as a script. If the code is being imported into another program, this block will not be executed.
+
+#The code then prints the positive and negative infinity in Python.
+
+#To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as infinity.py, you can run it by typing the following command into the command line:
+
