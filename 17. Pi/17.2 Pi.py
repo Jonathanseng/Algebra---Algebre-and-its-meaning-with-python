04453c3e1a58634b0be6c06eae70e8f9218db67c
Seng Keat <corodostudio@gmail.com>
2023-06-19 14:22:26 +0700

    Create 17.2 Pi.py

---
+++ 17. Pi/17.2 Pi.py
@@ -0,0 +1,23 @@
+import math
+
+def pi():
+  return math.pi
+
+def main():
+  print(pi())
+
+if __name__ == "__main__":
+  main()
+
+  #This code first imports the math module, which contains the mathematical constant pi. Then, it defines a function called pi() that returns the value of pi. Finally, it defines a main function that calls the pi() function and prints the result.
+
+#To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as pi.py, you can run it by typing the following command into the command line:
+
+#This will print the value of pi to the console.
+
+#Here is an explanation of the code:
+
+#The import math statement imports the math module. This module contains a number of mathematical functions, including the pi() function.
+#The pi() function defines a function that returns the value of pi. The math.pi constant returns the value of pi, which is approximately 3.141592653589793.
+#The main() function defines the main function of the program. This function calls the pi() function and prints the result.
+#The if __name__ == "__main__": statement defines a conditional block that is only executed when the code is run as a standalone program. This is a common way to write Python code, as it allows you to define functions that can be used from other programs.
