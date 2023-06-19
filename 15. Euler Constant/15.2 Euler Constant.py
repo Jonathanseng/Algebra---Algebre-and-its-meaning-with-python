26836cc99d63053234784ef185ee8a105afbc663
Seng Keat <corodostudio@gmail.com>
2023-06-19 14:03:35 +0700

    Create 15.2 Euler Constant.py

---
+++ 15. Euler Constant/15.2 Euler Constant.py
@@ -0,0 +1,24 @@
+import math
+
+
+def euler_constant():
+  """
+  Prints the value of Euler's constant in Python.
+
+  Returns:
+    None.
+  """
+
+  print("Euler's constant:", math.gamma)
+
+
+if __name__ == "__main__":
+  euler_constant()
+
+  # This code first imports the math module, which contains the definition of Euler's constant. Then, it defines a function called euler_constant() that prints the value of Euler's constant. The function uses the math.gamma constant to get the value of Euler's constant.
+
+#The if __name__ == "__main__": block at the end of the code is used to execute the code if it is being run as a script. If the code is being imported into another program, this block will not be executed.
+
+#The code then prints the value of Euler's constant.
+
+#To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as euler_constant.py, you can run it by typing the following command into the command line:
