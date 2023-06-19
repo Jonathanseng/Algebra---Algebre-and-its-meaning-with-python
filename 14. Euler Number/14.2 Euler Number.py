d52f7882142e56d6b3a4ffffd6a523275b90f57d
Seng Keat <corodostudio@gmail.com>
2023-06-19 14:02:00 +0700

    Create 14.2 Euler Number.py

---
+++ 14. Euler Number/14.2 Euler Number.py
@@ -0,0 +1,24 @@
+import math
+
+
+def euler_number():
+  """
+  Prints the value of Euler's number in Python.
+
+  Returns:
+    None.
+  """
+
+  print("Euler's number:", math.e)
+
+
+if __name__ == "__main__":
+  euler_number()
+
+  # This code first imports the math module, which contains the definition of Euler's number. Then, it defines a function called euler_number() that prints the value of Euler's number. The function uses the math.e constant to get the value of Euler's number.
+
+#The if __name__ == "__main__": block at the end of the code is used to execute the code if it is being run as a script. If the code is being imported into another program, this block will not be executed.
+
+#The code then prints the value of Euler's number.
+
+#To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as euler_number.py, you can run it by typing the following command into the command line:
