04649fb3e0183b87aab6c27e0eb76d2370334da3
Seng Keat <corodostudio@gmail.com>
2023-06-21 21:47:28 +0700

    Create 18.5 Universal Set.py

---
+++ 18. Set /18.5 Universal Set.py
@@ -0,0 +1,24 @@
+def universal_set():
+  """Returns the universal set."""
+  return set(range(100))
+
+def main():
+  """Prints the universal set."""
+  print(universal_set())
+
+if __name__ == "__main__":
+  main()
+
+#This code first defines a function called universal_set(). This function takes no arguments and returns a universal set. The universal set is created using the set() function with a range of numbers as the argument.
+
+#The next function is called main(). This function prints the universal set. The universal set is printed by calling the print() function on the output of the universal_set() function.
+
+#The last line of code specifies that the main() function is the entry point for the program. This means that when the program is run, the main() function will be executed first.
+
+#To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as universal_set.py, you can run it by typing the following command into the command line:
+
+# This will print the universal set to the console.
+
+#The output of the code will be a set of all the numbers from 0 to 99. This is because the range() function in Python returns a sequence of numbers, and the set() function creates a set from a sequence of numbers.
+
+#I hope this helps! Let me know if you have any other questions.
