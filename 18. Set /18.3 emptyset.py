51a4b22d737a8e34e12924699a24c45c1369481a
Seng Keat <corodostudio@gmail.com>
2023-06-21 21:44:28 +0700

    Create 18.3 emptyset.py

---
+++ 18. Set /18.3 emptyset.py
@@ -0,0 +1,18 @@
+def empty_set():
+  """Returns the empty set."""
+  return set()
+
+def main():
+  """Prints the empty set."""
+  print(empty_set())
+
+if __name__ == "__main__":
+  main()
+
+# This code first defines a function called empty_set(). This function takes no arguments and returns an empty set. The empty set is created using the set() function with no arguments.
+
+#The next function is called main(). This function prints the empty set. The empty set is printed by calling the print() function on the output of the empty_set() function.
+
+##The last line of code specifies that the main() function is the entry point for the program. This means that when the program is run, the main() function will be executed first.
+
+#To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as empty_set.py, you can run it by typing the following command into the command line:
