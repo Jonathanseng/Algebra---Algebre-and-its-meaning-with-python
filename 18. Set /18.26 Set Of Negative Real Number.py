9a4433802bb5b42086ad2c2b3bf61f68ee7a9b48
Seng Keat <corodostudio@gmail.com>
2023-06-23 19:19:27 +0700

    Update 18.26 Set Of Negative Real Number.py

--- 18. Set /18.26 Set Of Negative Real Number.py
+++ 18. Set /18.26 Set Of Negative Real Number.py
@@ -15,3 +15,13 @@ if __name__ == "__main__":
 #The main function of the code simply calls the set_of_negative_real_numbers() function and prints the result.
 
 #To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as set_of_negative_real_numbers.py, you can run it by typing the following command into the command line:
+
+# This will print the set of negative real numbers to the console.
+
+#Here is an explanation of the code:
+
+#The set_of_negative_real_numbers() function first defines an empty set called set_of_negative_real_numbers.
+#Then, the function iterates through all the numbers from negative infinity to 0. This is done using the range() function, which takes a start and end argument. In this case, the start argument is -float("inf"), which represents negative infinity. The end argument is 0.
+#For each number in the range, the function checks if the number is less than or equal to 0. If it is, the number is added to the set_of_negative_real_numbers set.
+#Finally, the function returns the set_of_negative_real_numbers set.
+#The main function of the code simply calls the set_of_negative_real_numbers() function and prints the result.
