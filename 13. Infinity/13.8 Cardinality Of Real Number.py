7ec2535c603f25f5eaaf563b99b54d49e42acb72
Seng Keat <corodostudio@gmail.com>
2023-06-25 19:50:33 +0700

    Create 13.8 Cardinality Of Real Number.py

---
+++ 13. Infinity/13.8 Cardinality Of Real Number.py
@@ -0,0 +1,19 @@
+def cardinality_of_real_number():
+  """Returns the cardinality of the real numbers."""
+  return float("inf")
+
+
+def main():
+  print(cardinality_of_real_number())
+
+
+if __name__ == "__main__":
+  main()
+
+#This code first defines a function called cardinality_of_real_number(). This function returns the cardinality of the real numbers, which is represented by float("inf") in Python.
+
+#The code then defines a function called main(). This function simply prints the cardinality of the real numbers.
+
+#Finally, the code executes the main() function.
+
+#To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as cardinality_of_real_number.py, you can run it by typing the following command into the command line:
