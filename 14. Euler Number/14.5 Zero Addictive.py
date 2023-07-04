dc923b7eb7e181d86820c38a07a25b602c104f11
Seng Keat <corodostudio@gmail.com>
2023-07-05 06:21:42 +0700

    Create 14.5 Zero Addictive.py

---
+++ 14. Euler Number/14.5 Zero Addictive.py
@@ -0,0 +1,15 @@
+def additive_identity(number):
+  """Returns the additive identity of the given number."""
+  return number + 0
+
+
+if __name__ == "__main__":
+  print(additive_identity(5))
+
+# This code first defines a function called additive_identity(). The function takes one number as input, and it returns the sum of that number and 0. The if __name__ == "__main__": block at the end of the code is used to run the function when the code is executed as a script. The code then prints the result of calling the additive_identity() function with the number 5 as input.
+
+#To run this code, you can save it as a Python file and then run the file from the command line. For example, if you save the code as identity.py, you can run it by typing the following command into the command line:
+
+# This will print the number 5 to the console, which is the result of calling the additive_identity() function with the number 5 as input.
+
+#As you can see, the Python code for zero additive identity is very similar to the Python code for one multiplicative identity. The only difference is that the additive_identity() function adds the number and 0, while the multiplicative_identity() function multiplies the number and 1.
