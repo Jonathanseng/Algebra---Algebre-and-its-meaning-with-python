bcdf71f7490a9869c39c25ca5a9114be28b28aed
Seng Keat <corodostudio@gmail.com>
2023-07-05 06:23:53 +0700

    Create 14.7 Multiplicative Identity.py

---
+++ 14. Euler Number/14.7 Multiplicative Identity.py
@@ -0,0 +1,11 @@
+def multiplicative_identity(number):
+  """Returns the multiplicative identity of the given number."""
+  return number * 1
+
+
+if __name__ == "__main__":
+  print(multiplicative_identity(5))
+
+# This code first defines a function called multiplicative_identity(). The function takes one number as input, and it returns the product of that number and 1. The if __name__ == "__main__": block at the end of the code is used to run the function when the code is executed as a script. The code then prints the result of calling the multiplicative_identity() function with the number 5 as input.
+
+#To run this code, you can save it as a Python file and then run the file from the command line. For example, if you save the code as identity.py, you can run it by typing the following command into the command line:
